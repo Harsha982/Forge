4c42f14ce24151d819f99c899acad8180aefc5e7
gowthamkumar12 <46594478+gowthamkumar12@users.noreply.github.com>
2022-12-23 07:35:34 +0000

    first commit

--- Frontend/Forge/src/app/app.module.ts
+++ Frontend/Forge/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LangingPageComponent } from './langing-page/langing-page.component';
+import { LandingPageHeaderComponent } from './landing-page-header/landing-page-header.component';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LangingPageComponent
+    LangingPageComponent,
+    LandingPageHeaderComponent,
+    LoginSignupComponent
   ],
   imports: [
     BrowserModule,
